d13291d03ffcb528fd56bc74582a8b841d8d02b3
anilcanbulut <anilcanbulut9@gmail.com>
2023-03-12 18:21:01 +0300

    added a new model-type parameter

--- src/lib/opts.py
+++ src/lib/opts.py
@@ -4,8 +4,9 @@ class Opts():
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='Turkish-TSL Translation')
         self.parser.add_argument('--data-path', '-p', default='configs/data.yaml', help='YAML fiel that stores dataset parameters')
-        self.parser.add_argument('--model-type', '-t', default='word', help='Type of the model either word or char')
-    
+        self.parser.add_argument('--token-type', '-t', default='word', help='Type of the token either word or char')
+        self.parser.add_argument('--model-type', '-m', default='transformer', help="This parameter allows to choose the model type either 'rnn' or 'transformer' can be given as input")
+
     def parse(self):
         args = self.parser.parse_args()
         return args
