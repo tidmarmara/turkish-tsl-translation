15c9b1e8f3728cef4f4330f72dc433ad89e8ebf1
anilcanbulut <anilcanbulut9@gmail.com>
2023-03-18 15:49:09 +0300

    added mask creation function

--- src/lib/models/transformer.py
+++ src/lib/models/transformer.py
@@ -33,6 +33,23 @@ def scaled_dot_product_attention(q, k, v, mask):
 
     return output, attention_weights
 
+def create_masks(inp, tar):
+    # Encoder padding mask
+    enc_padding_mask = create_padding_mask(inp)
+
+    # Used in the 2nd attention block in the decoder.
+    # This padding mask is used to mask the encoder outputs.
+    dec_padding_mask = create_padding_mask(inp)
+
+    # Used in the 1st attention block in the decoder.
+    # It is used to pad and mask future tokens in the input received by
+    # the decoder.
+    look_ahead_mask = create_look_ahead_mask(tf.shape(tar)[1])
+    dec_target_padding_mask = create_padding_mask(tar)
+    combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
+
+    return enc_padding_mask, combined_mask, dec_padding_mask
+
 def create_padding_mask(seq):
   seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
 
