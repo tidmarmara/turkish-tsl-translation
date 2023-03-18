b965793a36d9cf62e846130775ee9015a4ddb6c2
anilcanbulut <anilcanbulut9@gmail.com>
2023-03-18 19:10:40 +0300

    added tokenizer json paths and checkpoint path as new inputs

--- src/lib/opts.py
+++ src/lib/opts.py
@@ -3,6 +3,8 @@ import argparse
 class Opts():
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='Turkish-TSL Translation')
+
+        ################### TRAIN ###################
         self.parser.add_argument('--data-path', '-p', default='configs/data.yaml', help='YAML file that stores dataset parameters')
         self.parser.add_argument('--model-config', '-cfg', default='configs/networks/transformer.yaml', help='YAML fiel that stores the model paramters')        
         self.parser.add_argument('--token-type', '-t', default='word', help='Type of the token either word or char')
@@ -10,6 +12,11 @@ class Opts():
         self.parser.add_argument('--num-epochs', '-e', default=20, type=int, help='Number of epochs for the training')
         self.parser.add_argument('--batch-size', '-b', default=64, type=int, help='Batch size for the training')
         self.parser.add_argument('--root-path', '-r', default='results', type=str, help='Root path for the saved models')
+        
+        ################### TEST ###################
+        self.parser.add_argument('--inp-tokenizer', type=str, help='Direct path of the input tokenizer JSON file')
+        self.parser.add_argument('--targ-tokenizer', type=str, help='Direct path of the target tokenizer JSON file')
+        self.parser.add_argument('--ckpt-path', type=str, help='Direct path of the trained model checkpoints')
 
     def parse(self):
         args = self.parser.parse_args()
