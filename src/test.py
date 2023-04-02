27f192df1adda3fa6417b59ce5225a0f9869c2d5
anilcanbulut <anilcanbulut9@gmail.com>
2023-04-02 15:29:11 +0300

    added single sentence test for both transformer and rnn-based models

--- src/test.py
+++ src/test.py
@@ -3,18 +3,26 @@ from opts import Opts
 from loguru import logger
 from utils import Model
 from dataset import Dataset
+import os
 
 def main(opts):
     dataset = Dataset(opts)
-    input_tokenizer = dataset.load_tokenizer(opts.inp_tokenizer)
-    target_tokenizer = dataset.load_tokenizer(opts.targ_tokenizer)
+    input_tokenizer = dataset.load_tokenizer(os.path.join(opts.exp_path, 'inp_tokenizer.json'))
+    target_tokenizer = dataset.load_tokenizer(os.path.join(opts.exp_path, 'targ_tokenizer.json'))
 
     model_creator = Model(opts, dataset)
     model = model_creator.get_model(input_tokenizer, target_tokenizer)
-    model = model_creator.load_model(model, opts.ckpt_path)
+    model = model_creator.load_model(model, os.path.join(opts.exp_path, 'ckpts', 'best'))
+
+    model_type = model_creator.model_config['experiment-parameters']['model-type'].lower()
     
-    sentence = "sen de gelmek istiyor musun?"
-    pred, _ = model_creator.predict_sentence(model, sentence, input_tokenizer, target_tokenizer, 50)
+    sentence = "film nasıldı?"
+
+    if model_type == "rnn-based":
+        pred, sentence, _ = model_creator.evaluate(input_tokenizer, target_tokenizer, sentence, model, 14)
+    elif model_type == "transformer":
+        pred, _ = model_creator.predict_sentence(model, sentence, input_tokenizer, target_tokenizer, 20)
+    
     logger.success(f"Predicted sentence: {pred}")
 
 if __name__ == '__main__':
