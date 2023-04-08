89cf1921c9fe9d218114e1dbc4fc9c4319536710
anilcanbulut <anilcanbulut9@gmail.com>
2023-04-08 09:09:00 +0300

    added validation step parameter as input

--- src/lib/opts.py
+++ src/lib/opts.py
@@ -11,6 +11,7 @@ class Opts():
         self.parser.add_argument('--num-epochs', '-e', default=20, type=int, help='Number of epochs for the training')
         self.parser.add_argument('--batch-size', '-b', default=64, type=int, help='Batch size for the training')
         self.parser.add_argument('--root-path', '-r', default='results', type=str, help='Root path for the saved models')
+        self.parser.add_argument('--validation-step', '-s', type=int, default=5, help='Specifies after how many epochs to apply validation')
         
         ################### TEST ###################
         self.parser.add_argument('--exp-path', type=str, help='root path for the experiment folder that contains checkpoints, and tokenizer JSON files')
